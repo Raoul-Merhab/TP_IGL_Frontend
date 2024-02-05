815f9e37d4008e494db8e851639e4e789c423467
Raoul <la_merhab@esi.dz>
2024-02-05 22:01:13 +0100

    BackEnd endpoints

--- src/api/Urls.js
+++ src/api/Urls.js
@@ -2,6 +2,18 @@ const Urls = {
     GET_ROOT:"/",
     SIGN_IN:"/auth/login",
     SIGN_UP:"/auth/signup",
+    MY_ACCOUNT:"/auth/my-account",
+    MY_ACCOUNT_EDIT:"/auth/update-account",
+    // admin endpoints
+    GET_MODS:"/admin/liste-des-moderateurs",
+    GET_ARTICLES:"/admin/liste-des-articles",
+    AJOUTER_MOD:"/admin/ajouter-moderateur",
+    SUPPRIMER_MOD:"/admin/supprimer-moderateur",
+    // user endpoints
+    GET_FAVORIS:"/user/articles-favoris",
+    ADD_FAVORI:"/user/ajouter-article-favoris",
+    DOWNLOAD_FAVORI:"/user/telecharger-article-favoris",
+    DELETE_FAVORI:"/user/supprimer-article-favoris"
 }
 
 export default Urls
